9df4958a9ef9eeaf374810ce5544c2dcba04bf11
MaciejSpalek <spwrtt@gmail.com>
2020-02-08 17:25:29 +0100

    start developing game mechanics

--- src/Components/game.js
+++ src/Components/game.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 
-class Board extends React.Component {
+class Board extends Component {
     constructor(props) {
         super(props);
         this.state = {
             board: [],
-            dimension: 10
+            dimension: 10,
+            firstSquare: null
         }
+        this.buttonListener = this.buttonListener.bind(this); 
     } 
     componentDidMount() {
         this.setState({
             board: this.myList()
         })
+        
     }
     myList() {
         let list = []
@@ -22,9 +25,23 @@ class Board extends React.Component {
         }
         return list;
     }
-    
+    getRandom() {
+        return Math.round(Math.random()*this.state.board.length)
+    }
+    drawFirstSquare() {
+        return this.state.board[this.getRandom()];
+    }
+    buttonListener() {
+        this.drawFirstSquare();
+        this.setState(prevState => ({
+            firstSquare: prevState.firstSquare
+        }))
+        console.log(this.state.firstSquare);
+    }
     render() {
+        
         return (
+            <>
             <div className="board">
                 {this.state.board.map((value) => {
                     return (
@@ -32,29 +49,36 @@ class Board extends React.Component {
                     )
             })}
             </div>
+            <button onClick = {this.buttonListener}> Start </button>
+            </>
         )
     }
 }
 
-class Square extends React.Component  {
+
+
+
+
+
+
+class Square extends Component {
     constructor(props) {
         super(props);
         this.handleSelect = this.handleSelect.bind(this); 
     }
-
     handleSelect() {
-        // console.log(this.props.value);
-        console.log(this.getColumn());
+        // console.log(this.getColumn());
+        
     }
-
+    getRandom() {
+        return Math.round(Math.random()*3)
+    }
     getRow() {
         return this.props.value.substr(0, 1);
     }
-
     getColumn() {
         return this.props.value.substr(2, 1);
     }
-
     render() {
         return (
             <div className="square" onClick={this.handleSelect}></div>
