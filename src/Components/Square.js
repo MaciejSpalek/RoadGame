123be902b2e30ea2657e259517dec0b0ba6c3370
MaciejSpalek <spwrtt@gmail.com>
2020-02-10 14:08:29 +0100

    wip: develop getNewSquare()

---
+++ src/Components/Square.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSelect = this.handleSelect.bind(this);
+  }
+  handleSelect() {
+    console.log(this.props.firstSquare);
+  }
+  getDirection() {
+    return Math.round(Math.random() * 3);
+  }
+ 
+  render() {
+    const { firstSquare, row, col, key } = this.props;
+   
+    return (<div className={firstSquare === `${row}${col}` ? "startSquare" : "square"} onClick={this.handleSelect} col={col} row={row}></div>);
+  }
+}
--- src/Components/game.js
+++ src/Components/game.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Square } from "./Square";
 
 class Board extends React.Component {
   constructor(props) {
@@ -6,73 +7,94 @@ class Board extends React.Component {
     this.buttonListener = this.buttonListener.bind(this);
     this.state = {
       board: [],
-      secondBoard: [],
       road: [],
       dimension: 10,
+      amountOfSquares: 5,
       firstSquare: null
     };
-    
   }
 
   componentDidMount() {
     this.setState({
-      board: this.myList()
+      board: this.createBoard()
     });
   }
   
-  myList() {
+  createBoard() {
     let list = [];
     for (let row = 0; row < this.state.dimension; row++) {
       list[row] = [];
       for (let col = 0; col < this.state.dimension; col++) {
-        list[row][col] = `${row}-${col}`;
+        list[row][col] = `${row}${col}`;
       }
     }
     return list;
   }
 
   getRandom() {
-    return this.state.board[
-      Math.round(Math.random() * this.state.board.length)
-    ];
+    return this.state.board[Math.round(Math.random()*9)][Math.round(Math.random()*9)];
   }
-
   drawFirstSquare() {
     let randomFiled = this.getRandom();
-    this.setState({firstSquare: randomFiled}, console.log(randomFiled));
+    this.setState({firstSquare: randomFiled});
+    this.getNewSquare(randomFiled);
     
+    console.log(this.state.road);
   }
-
-  // boardLoop() {
-  //   this.state.board.map(square => {
-  //     if()
-  //   })
-  // }
 
   getDirection() {
     return Math.round(Math.random() *3);
   }
-  createRoad() {
-    const direction = this.getDirection();
+
+  getNewSquare(lastSquare) {
+    const { road, board } = this.state;
+
+    for(let i=0; i<this.state.amountOfSquares; i++) {
+      const direction = this.getDirection();
+      const row = lastSquare.substr(0, 1);
+      const col = lastSquare.substr(1, 1);
     
-    if(direction == 0) {
-      console.log("up")
-      
-    } 
+    // console.log(lastSquare, row, col)
+    
+      if(direction == 0) {
+        console.log("up")
+        if(row-1<=0) {
+          road.push(board[row-1][col])
+          return lastSquare = `${row-1}${col}`
+        } else {
+          this.getNewSquare(lastSquare)
+        }
+      } 
 
-    else if(direction == 1) {
-      console.log("right")
+      else if(direction == 1) {
+        console.log("right")
+        if(col+1<=9) {
+          road.push(board[row][col+1])
+          return lastSquare = `${row}${col+1}`
+        } else {
+          this.getNewSquare(lastSquare)
+        }
+      }
 
-    }
+      else if(direction == 2) {
+        console.log("down")
+        if(row+1<=9) {
+          road.push(board[row+1][col])
+          return lastSquare = `${row+1}${col}`
+        } else {
+          this.getNewSquare(lastSquare)
+        }
+      } 
 
-    else if(direction == 2) {
-      console.log("down")
-
-    } 
-
-    else {
-      console.log("left")
-
+      else {
+        console.log("left")
+        if(col-1>=0) {
+          road.push(board[row][col-1])
+          return lastSquare = `${row}${col-1}`
+        } else {
+          this.getNewSquare(lastSquare)
+        }
+      }
     }
   }
 
@@ -80,33 +102,13 @@ class Board extends React.Component {
   buttonListener() {
     this.drawFirstSquare();
   }
-
-
-
-
-
-
-
-
-
-
-  // return grid.map((row, i) => {
-  //   return row.map((square, j) => {
-  //     return (
-  //       <ShipGridSquare
-  //         key={`${i}${j}`}
-  //         i={i}
-  //         j={j}
-  //         shipsSet={shipsSet}
-  //         square={square}
- 
   renderSquares() {
     const { firstSquare, board } = this.state;
     return board.map((row, i) => {
-      return row.map((square, j) => {
+      return row.map((col, j) => {
         return (
           <Square 
-            firstSquare={firstSquare === square ? firstSquare : null}
+            firstSquare={firstSquare === col ? firstSquare : null}
             key={`${i}${j}`}
             row={i} 
             col={j} 
@@ -128,37 +130,6 @@ class Board extends React.Component {
           <button className="game__start-button" onClick={this.buttonListener}> Start </button>
         </div>
       </>
-    );
-  }
-}
-
-class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleSelect = this.handleSelect.bind(this);
-  }
-  handleSelect() {
-    console.log(this.props.firstSquare);
-  }
-  getRandom() {
-    return Math.round(Math.random() * 3);
-  }
-  getRow() {
-    return this.props.value.substr(0, 1);
-  }
-  getColumn() {
-    return this.props.value.substr(2, 1);
-  }
-  render() {
-    const { firstSquare, row, col } = this.props;
-    console.log(row, col)
-    return (
-      <div
-        className={firstSquare === col ? "startSquare" : "square"}
-        onClick={this.handleSelect}
-        col = {col}
-        row = {row}
-      ></div>
     );
   }
 }
