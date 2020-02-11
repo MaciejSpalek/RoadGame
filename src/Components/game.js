4df36d120d682c668389c61ee63ff18d84aac8e9
MaciejSpalek <spwrtt@gmail.com>
2020-02-11 15:50:56 +0100

    feat: finish function responsible for creating road

--- src/Components/game.js
+++ src/Components/game.js
@@ -10,8 +10,9 @@ class Board extends React.Component {
       road: [],
       dimension: 10,
       amountOfSquares: 5,
-      firstSquare: null
+      firstSquare: null,
     };
+
   }
 
   componentDidMount() {
@@ -19,7 +20,6 @@ class Board extends React.Component {
       board: this.createBoard()
     });
   }
-
   createBoard() {
     let list = [];
     for (let row = 0; row < this.state.dimension; row++) {
@@ -30,105 +30,118 @@ class Board extends React.Component {
     }
     return list;
   }
-
   getRandom() {
     return this.state.board[Math.round(Math.random() * 9)][Math.round(Math.random() * 9)];
   }
-  componentDidUpdate() {
-
-    // for (let i = 0; i < this.state.amountOfSquares; i++) {
-    //   this.getNewSquare(this.state.firstSquare)
-    // }
-    //
+  drawFirstSquare = async ()=> {
     let randomFiled = this.getRandom();
-    if (this.state.firstSquare === null) {
-      this.setState({ firstSquare: randomFiled });
+    await this.setState({ firstSquare: randomFiled })
+    await this.setRoad(randomFiled)
+  }
+
+  isFirstSquare(row, col) {
+    
+    return this.state.firstSquare == `${row}${col}`;
+  }
+
+  setRoad = async firstSquare => {
+    const roadArray = [];
+    for(let i=0; i<this.state.amountOfSquares; i++) {
+      this.setSingleSquare(roadArray, firstSquare, i)
+    }
+    await this.setState({road: roadArray})
+  }
+
+  isBusySquare(roadArray, row, col) {
+    for(let i=0; i<roadArray.length; i++) {
+      if(roadArray[i] == `${row}${col}`) {
+        return true;
+      }
     }
   }
 
-  drawFirstSquare() {
-    // console.log(randomFiled);
-    console.log(this.state.firstSquare);
+  setSingleSquare(roadArray, firstSquare, i) {
+    console.log(`~~~ START FUNC ~~~`);    
+        const { board } = this.state;
+        const direction = this.getDirection();
+        let row;
+        let col;
+        
 
-
-    this.getNewSquare(this.state.firstSquare);
-
+        if(i < 1) {
+            row = +firstSquare.substr(0, 1);
+            col = +firstSquare.substr(1, 1);
+        } else {
+            row = +roadArray[roadArray.length-1].substr(0, 1);
+            col = +roadArray[roadArray.length-1].substr(1, 1);
+        }
+        
+    
+        if (direction === 0) {
+          
+          if (row - 1 >= 0 && !this.isBusySquare(roadArray, row-1, col) && !this.isFirstSquare(row-1, col)) {
+            console.log("up"+firstSquare)
+            firstSquare = board[row - 1][col]
+            roadArray.push(firstSquare)
+            console.log(roadArray)
+          } 
+  
+          else {
+            console.log(`up - return function`)
+            return this.setSingleSquare(roadArray, firstSquare, i)
+            
+          }
+        }
+    
+        else if (direction === 1) {
+          if (col + 1 <= 9 && !this.isBusySquare(roadArray, row, col+1) && !this.isFirstSquare(row, col+1)) {
+            console.log("right"+firstSquare)
+            firstSquare = board[row][col + 1];
+            roadArray.push(firstSquare)
+            console.log(roadArray)
+          } 
+          else {
+            console.log(`right - return function`)
+            return this.setSingleSquare(roadArray, firstSquare, i)
+          }
+        }
+  
+        else if (direction === 2) {
+          if (row + 1 <= 9 && !this.isBusySquare(roadArray, row+1, col) && !this.isFirstSquare(row+1, col)) {
+            console.log("down"+firstSquare)
+            firstSquare = board[row + 1][col]
+            roadArray.push(firstSquare)
+            console.log(roadArray)
+          } 
+          else {
+            console.log(`down - return function`)
+            return this.setSingleSquare(roadArray, firstSquare, i)
+          }
+        }
+    
+        else if (direction === 3) {
+          if (col - 1 >= 0 && !this.isBusySquare(roadArray, row, col-1) && !this.isFirstSquare(row, col-1)) {
+            console.log("left"+firstSquare)
+            firstSquare = board[row][col - 1]
+            roadArray.push(firstSquare)
+            console.log(roadArray)
+          } 
+          else {
+            console.log(`left - return function`)
+            return this.setSingleSquare(roadArray, firstSquare, i)
+          }
+        }
+        
   }
 
   getDirection() {
     return Math.round(Math.random() * 3);
   }
 
-  getNewSquare(lastSquare) {
-    const { road, board } = this.state;
-    // for (let i = 0; i < this.state.amountOfSquares; i++) {
-    // }
-
-    const direction = this.getDirection();
-    const row = lastSquare.substr(0, 1);
-    const col = lastSquare.substr(1, 1);
-
-    // console.log(lastSquare, row, col)
-
-    if (direction === 0) {
-      console.log("up")
-      if (row - 1 <= 0) {
-        road.push(board[row - 1][col])
-        this.setState({
-          firstSquare: board[row - 1][col]
-        })
-        console.log(this.state.firstSquare);
-      } else {
-        this.getNewSquare(lastSquare)
-      }
-    }
-    else if (direction === 1) {
-      console.log("right")
-      if (col + 1 <= 9) {
-        road.push(board[row][col + 1])
-        this.setState({
-          firstSquare: board[row][col + 1]
-        })
-        console.log(this.state.firstSquare);
-
-      } else {
-        this.getNewSquare(lastSquare)
-      }
-    }
-    else if (direction === 2) {
-      console.log("down")
-      if (row + 1 <= 9) {
-        road.push(board[row + 1][col])
-        this.setState({
-          firstSquare: board[row + 1][col]
-        })
-        console.log(this.state.firstSquare);
-
-      } else {
-        this.getNewSquare(lastSquare)
-      }
-    }
-    else {
-      console.log("left")
-      if (col - 1 >= 0) {
-        road.push(board[row][col - 1])
-        this.setState({
-          firstSquare: board[row][col - 1]
-        })
-        console.log(this.state.firstSquare);
-
-      } else {
-        this.getNewSquare(lastSquare)
-      }
-    }
-    console.log(this.state.firstSquare);
-    return this.state.firstSquare
-  }
-
-
   buttonListener() {
     this.drawFirstSquare();
   }
+
   renderSquares() {
     const { firstSquare, board } = this.state;
     return board.map((row, i) => {
