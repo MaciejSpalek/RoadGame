267a0870bef31904687ba043c327db0a5c5bff12
PiotrGrobelak <grobelak.piotr@gmail.com>
2020-02-14 18:57:02 +0100

    feat: added setTimeout for remove road from board

--- src/Components/Square.js
+++ src/Components/Square.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom"
 import classNames from "classnames";
 
 export class Square extends React.Component {
@@ -7,34 +6,40 @@ export class Square extends React.Component {
     super(props);
     this.state = {
       draw: "",
-      isRunning: false
+      isVisible: false,
+      road: props.road,
+      isRunning: props.isStarted
     }
-    this.tick = null
+    this.timeForDrawId = null
+    this.timeForHideId = null;
     this.handleSelect = this.handleSelect.bind(this);
   }
   handleSelect() {
     console.log(this.props.firstSquare);
     console.log(this.props.partOfRoad);
   }
-  componentDidUpdate(prevProps, prevState) {
-  }
-  setDrawRoad() {
-    this.setState({
-      draw: "drawRoad",
-      isRunning: true
-    })
-  }
-  wait(duration) {
-    if (this.props.isStarted) {
-      this.tick = setTimeout(() => {
-        this.setDrawRoad()
-      }, duration);
-    }
-    if (this.tick > 30) {
-      window.clearTimeout(this.tick)
+  async componentDidUpdate() {
+    if (this.state.isRunning) {
+      this.hideRoad()
     }
   }
-  componentDidMount() {
+  setDrawRoad(newState, duration) {
+    if (this.state.isRunning) {
+      return new Promise((resolve) => {
+        this.timeForDrawId = setTimeout(() => {
+          this.setState(newState, () => {
+            resolve()
+          })
+        }, duration)
+      })
+    }
+  }
+  async wait(duration) {
+    await this.setDrawRoad({ draw: "drawRoad", isVisible: true }, duration)
+    if (this.timeForDrawId > 30) {
+      window.clearTimeout(this.timeForDrawId)
+      this.timeForDrawId = null
+    }
   }
   updateRoad() {
     const { row, col, partOfRoad, duration } = this.props;
@@ -42,22 +47,40 @@ export class Square extends React.Component {
       this.wait(duration.filter(el => typeof el == "number" ? el : null)[0]);
     }
   }
+  hideRoad() {
+    return new Promise((resolve, reject) => {
+      this.timeForHideId = setTimeout(() => {
+        this.setState({
+          draw: "",
+          isVisible: false,
+          isRunning: false
+        })
+      }, 5000)
+      if (!this.state.isRunning) {
+        window.clearTimeout(this.timeForHideId)
+        this.timeForHideId = null;
+      }
+    })
+  }
   renderSquares = () => {
     this.updateRoad()
-    const { firstSquare, row, col, partOfRoad, duration } = this.props;
+    const { draw, isVisible } = this.state;
+    const { firstSquare, row, col, partOfRoad, duration, index } = this.props;
     const squareClass = classNames({
       'square': true,
       'startSquare': firstSquare === `${row}${col}`,
-      'drawRoad': partOfRoad[0] === `${row}${col}` ? this.state.draw : null
+      'drawRoad': isVisible ? partOfRoad[0] === `${row}${col}` ? draw : null : false
     })
     return (
       <div
+        index={index}
         className={squareClass}
         col={col}
         row={row}
         duration={duration}
         onClick={this.handleSelect}
       >
+        {index}
       </div >
     );
   }
