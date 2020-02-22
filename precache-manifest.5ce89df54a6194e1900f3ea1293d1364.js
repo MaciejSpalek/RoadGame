aa54dc09ad7a1df4fb0a37489d185b52e7414076
MaciejSpalek <spwrtt@gmail.com>
2020-02-22 20:02:01 +0100

    Updates

---
+++ precache-manifest.5ce89df54a6194e1900f3ea1293d1364.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9377a99b09a575ae90c0ccb288dc8fc",
+    "url": "/RoadGame/index.html"
+  },
+  {
+    "revision": "da0f15d5fadaf1f01fc1",
+    "url": "/RoadGame/static/css/main.9635b44d.chunk.css"
+  },
+  {
+    "revision": "1e0a8fb3e34279e20a40",
+    "url": "/RoadGame/static/js/2.2c08d55c.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/RoadGame/static/js/2.2c08d55c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da0f15d5fadaf1f01fc1",
+    "url": "/RoadGame/static/js/main.9a89d863.chunk.js"
+  },
+  {
+    "revision": "9c4c8f68701167efa842",
+    "url": "/RoadGame/static/js/runtime-main.0e1caf29.js"
+  },
+  {
+    "revision": "606602c807af49635c631717f3b2143c",
+    "url": "/RoadGame/static/media/bgImage.606602c8.jpg"
+  }
+]);
