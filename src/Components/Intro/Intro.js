efc3fe997b245ecc6f4fcbb68f2638fd1fda6485
PiotrGrobelak <grobelak.piotr@gmail.com>
2020-02-28 20:02:27 +0100

    feat: add styles for view on mobile,tablet,desktop

--- src/Components/Intro/Intro.js
+++ src/Components/Intro/Intro.js
@@ -18,22 +18,22 @@ export default function Intro() {
                     <li className="introduction__item">
                         <span className="introduction__step">Step 1</span>
                         <img className="introduction__image" src={step_1} alt="introduction_image" />
-                        <p className="introduction__description">At the beginning, you choose the game level<span role="img" aria-label="emoji">&#128518;</span></p>
+                        <p className="introduction__description">At the beginning, choose the game level.</p>
                     </li>
                     <li className="introduction__item">
                         <span className="introduction__step">Step 2</span>
                         <img className="introduction__image" src={step_2} alt="introduction_image" />
-                        <p className="introduction__description">OK! <span role="img" aria-label="emoji">&#128526;</span> Click start and look at the board where AI drawing road and remember in order road parts</p>
+                        <p className="introduction__description">OK! Click start and look at the board where AI drawing road and remember in order road parts</p>
                     </li>
                     <li className="introduction__item">
                         <span className="introduction__step">Step 3</span>
                         <img className="introduction__image" src={step_3} alt="introduction_image" />
-                        <p className="introduction__description">Next, click in order parts of the road. Warning! If you miss or click in not order, you lose lives<span role="img" aria-label="emoji">&#128534;</span></p>
+                        <p className="introduction__description">Next, click in order parts of the road. Warning! If you miss or click in not order, you lose lives</p>
                     </li>
                     <li className="introduction__item">
                         <span className="introduction__step">Step 4</span>
                         <img className="introduction__image" src={step_4} alt="introduction_image" />
-                        <p className="introduction__description">If you click in order all of the road parts, you are going to the next board with one additional part of the road. All levels have ten boards level. If you win all levels you are an expert and you don't need to eat nuts! Have fun <span role="img" aria-label="emoji"> &#128522;</span></p>
+                        <p className="introduction__description">If you click in order all of the road parts, you are going to the next board with one additional part of the road. All levels have ten boards level. If you win all levels you are an expert and you don't need to eat nuts! Have fun</p>
                     </li>
                 </ul>
                 <button className="introduction__button"><img className="introduction__button-icon" src={close} alt="close-icon"></img></button>
